34f127ab2e38ed54897a1e543ff8b8feceed7ef8
Stefano Zacchiroli <zack@upsilon.cc>
2023-11-05 13:30:13 +0100

    test_sig_sorter: limit max number of used files to 512 (down from 1024)
    This avoids having the test fail on machine with the (rather common) limit of
    1024 open files.

--- src/utils/sig_store.rs
+++ src/utils/sig_store.rs
@@ -441,7 +441,7 @@ impl<T: ZeroCopy> SigStore<T> {
 fn test_sig_sorter() {
     use rand::prelude::*;
     for max_chunk_bits in [0, 2, 8, 10] {
-        for buckets_high_bits in [0, 2, 8, 10] {
+        for buckets_high_bits in [0, 2, 8, 9] {
             for chunk_high_bits in [0, 2, 8, 10] {
                 if chunk_high_bits > max_chunk_bits {
                     continue;
